6cc4cdf5cb3fbe890fd23da133191dbb3758ac4a
deryJ13 <dery.j13@gmail.com>
2019-07-23 17:37:31 -0400

    Generate team resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_201555) do
+ActiveRecord::Schema.define(version: 2019_07_23_211151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2019_07_23_201555) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "pokemon_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pokemon_id"], name: "index_teams_on_pokemon_id"
+    t.index ["user_id"], name: "index_teams_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "token", null: false
@@ -43,4 +52,6 @@ ActiveRecord::Schema.define(version: 2019_07_23_201555) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "teams", "pokemons"
+  add_foreign_key "teams", "users"
 end
